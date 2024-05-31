78a48443a05910a879116e38697871eb7e185380
ditya0398 <aditya.maheshwari033@gmail.com>
2024-05-31 23:53:56 +0530

    refactored ppm template

--- 01-PPMImage/main.cpp
+++ 01-PPMImage/main.cpp
@@ -14,7 +14,7 @@ int main(){
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.25;
-
+            
             int ir =  static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);

952349c7d9aef1e289f0f9503abffa4c7752f6a2
ditya0398 <aditya.maheshwari033@gmail.com>
2024-07-06 17:29:37 +0530

    added the antialiasing with multiple rays logic

---
+++ src/08-Antialiasing/main.cpp
@@ -0,0 +1,29 @@
+// Command used to compile on MAC : 
+// 1) clang++  -std=c++11 main.cpp -o test
+// 2) ./test > img.ppm
+
+#include "rt_weekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "camera.h"
+
+#include <iostream>
+
+
+int main() {
+   
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    camera cam;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.render(world);
+    std::clog << "\rDone.                 \n";
+}
